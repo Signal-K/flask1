ecc6daa06ee45811ae9df5962aedab328a8af56c
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-02-16 02:53:35 +0000

    DSP-10: Up to Unit Testing the User Model in part 8.2

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
-from flask_login import login_user, logout_user, current_user, login_required 
-import logging
-from logging.handlers import SMTPHandler
-from logging.handlers import RotatingFileHandler
-import os
+#from app import routes, models, errors
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,8 +14,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-
-from app import routes, models, errors
 
 if not app.debug: # Only enable the email logger when the app is running without debug mode
     if app.config['MAIL_SERVER']:
@@ -40,8 +37,8 @@ if not app.debug: # Only enable the email logger when the app is running without
     backupCount=10)
     file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
 
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('Microblog startup)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
--- app/routes.py
+++ app/routes.py
@@ -54,7 +54,7 @@ def user(username):
 @app.route('/edit_profile')
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data

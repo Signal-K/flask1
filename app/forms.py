53321657d6832dc84858c41defb1034911cadd2f
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-02-17 05:54:00 +0000

    DSP-10: Finish part 8 followers

--- app/forms.py
+++ app/forms.py
@@ -24,3 +24,7 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError('Please use a different username')
+
+# Integrating Followers with the Application - Part 8
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,7 @@ from flask_login import logout_user
 from app.models import User
 from datetime import datetime
 from app.forms import EditProfileForm
+from app.forms import EmptyForm
 
 @app.before_request
 def before_request():
@@ -66,3 +67,44 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile',
                            form=form)
+
+# Integrate followers and create a follow page
+# ALlow a user to follow another user
+@app.route('/follow/<username>', methods=['POST']) # Post result of a form which is to follow a user from another user
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first() # the user that the account page belongs to, not the logged in user
+        if user is None: # If the user that the logged in user is trying to follow doesn't exist
+            flash('user {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user: # if the account page's user is the same as the logged in user (l.i.u = current_user)
+            flash('You cannot follow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.follow(user) # use the user's "follow" function to follow the user who owns the current page
+        db.session.commit() # add this latest development/update to the database
+        flash('You are following {}!'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
+    return render_template('user.html', user=user, posts=posts, form=form)
+# Allow a user to unfollow another user
+@app.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot unfollow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.unfollow(user)
+        db.session.commit()
+        flash('You are not following {}.'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))

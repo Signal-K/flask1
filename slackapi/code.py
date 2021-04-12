8ab5cb34c3ad278ff6bbdeaf1309c95c9cd4e906
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-04-12 11:10:47 +0000

    DSP-22: Code link, stel/glitch info, virtualenv for slack api setup

---
+++ slackapi/code.py
@@ -0,0 +1,7 @@
+python # bash
+>>> from slackclient import SlackClient
+>>> slack_client = SlackClient('your test token here')
+>>> slack_client.api_call("api.test")
+
+# https://www.notion.so/skinetics/Slack-API-5cbe02421423410ca4a1c2f731df56e2
+# http://ar.skinetics.tech/stellarios/compass - Python section

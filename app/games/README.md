ad5980d12ca873db0bc2be7aa644e3717f4f8ec5
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-04-23 13:20:57 +0000

    _

--- app/games/README.md
+++
@@ -1,2 +0,0 @@
-Code for embedding Unity games in a Flask HTML Template:
-https://github.com/acord-robotics/acord-robotics.github.io/tree/024f55d143a8008d1773095ebb1b02282653fd99/account/dashboard/msp
--- src/lib/consts.ts
+++
@@ -1,9 +0,0 @@
-export const INDENT = '&nbsp;&nbsp;&nbsp;&nbsp;';
-
-export const LINE_BREAKE_TAG = '<br>';
-
-export const NO_ROOM_PATTERN = 'No roomId for ';
-
-export const TIMEOUT = 60000;
-
-export const END_NO_ROOM_PATTERN = ' from Matrix';
